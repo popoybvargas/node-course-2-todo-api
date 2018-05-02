95782ae2d572f81284f6a65278088179188c42a7
Popoy Vargas <ipopoyMM@popoy-mm.local>
2018-05-02 17:29:45 +0800

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const { ObjectID } = require( 'mongodb' );
+
 var express = require( 'express' );
 var bodyParser = require( 'body-parser' );
 
@@ -33,6 +35,21 @@ app.get( '/todos', ( req, res ) =>
 	}, ( e ) =>
 	{
 		res.status( 400 ).send( e );
+	});
+});
+
+app.get( '/todos/:id', ( req, res ) =>
+{
+	var id = req.params.id;
+
+	if ( ! ObjectID.isValid( id ) ) return res.status( 404 ).send();
+
+	Todo.findById( id ).then( ( todo ) =>
+	{
+		todo ? res.send( { todo } ) : res.status( 404 ).send();
+	}, ( e ) =>
+	{
+		res.status( 400 ).send();
 	});
 });
 
