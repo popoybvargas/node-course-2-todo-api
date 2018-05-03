e4a4e97a4d1e563890f3ba45944a633d48ca9b26
Popoy Vargas <ipopoyMM@popoy-mm.local>
2018-05-03 15:44:28 +0800

    added heroku MONGODB_URI config var

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,6 @@ var mongoose = require( 'mongoose' );
 
 mongoose.Promise = global.Promise;
 
-// mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp' );
-mongoose.connect( 'mongodb://popoyvargas:trudg1ng-trampLes@ds113640.mlab.com:13640/node-todo-api' || 'mongodb://localhost:27017/TodoApp' );
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp' );
 
 module.exports = { mongoose };
