6ffe831e7875c29639f0e789648905ea1f7c7b1c
Popoy Vargas <ipopoyMM@popoy-mm.local>
2018-05-04 12:32:38 +0800

    added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
+const _ = require( 'lodash' );
 const { ObjectID } = require( 'mongodb' );
-
-var express = require( 'express' );
-var bodyParser = require( 'body-parser' );
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
 
 var { mongoose } = require( './db/mongoose' );
 var { Todo } = require( './models/todo' );
@@ -65,6 +65,32 @@ app.delete( '/todos/:id', ( req, res ) =>
 	{
 		todo ? res.send( { todo } ) : res.status( 404 ).send( 'Todo does not exist!' );
 	}, ( e ) =>
+	{
+		res.status( 400 ).send();
+	});
+});
+
+app.patch( '/todos/:id', ( req, res ) =>
+{
+	var id = req.params.id;
+	var body = _.pick( req.body, [ 'text', 'completed' ] );
+
+	if ( ! ObjectID.isValid( id ) ) return res.status( 404 ).send( 'Invalid ID!' );
+
+	if ( _.isBoolean( body.completed ) && body.completed )
+	{
+		body.completedAt = new Date().getTime();
+	}
+	else
+	{
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate( id, { $set: body }, { new: true } ).then( ( todo ) =>
+	{
+		! todo ? res.status( 404 ).send() : res.send( { todo } );
+	}).catch( ( e ) =>
 	{
 		res.status( 400 ).send();
 	});
