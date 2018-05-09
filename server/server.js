b0fcfa5cb9f4b5517467bcce30ec81987591e391
Popoy Vargas <ipopoyMM@popoy-mm.local>
2018-05-09 13:06:06 +0800

    added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require( 'lodash' );
 const { ObjectID } = require( 'mongodb' );
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
+const bcrypt = require( 'bcryptjs' );
 
 var { mongoose } = require( './db/mongoose' );
 var { Todo } = require( './models/todo' );
@@ -120,6 +121,19 @@ app.post( '/users', ( req, res ) =>
 app.get( '/users/me', authenticate, ( req, res ) =>
 {
 	res.send( req.user );
+});
+
+app.post( '/users/login', ( req, res ) =>
+{
+	var body = _.pick( req.body, [ 'email', 'password' ] );
+
+	User.findByCredentials( body.email, body.password ).then( ( user ) =>
+	{
+		return user.generateAuthToken().then( ( token ) =>
+		{
+			res.header( 'x-auth', token ).send( user );
+		});
+	}).catch( ( e ) => res.status( 400 ).send() );
 });
 
 app.listen( port, () => console.log( `Started on port ${ port }` ) );
