41792ba77297e26ba2e873281bef2519a5cd05b2
Popoy Vargas <ipopoyMM@popoy-mm.local>
2018-05-09 10:59:20 +0800

    hashed user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,24 @@
 const { SHA256 } = require( 'crypto-js' );
 const jwt = require( 'jsonwebtoken' );
+const bcrypt = require( 'bcryptjs' );
 
+var password = '123abc!';
+
+// bcrypt.genSalt( 10, ( err, salt ) =>
+// {
+// 	bcrypt.hash( password, salt, ( err, hash ) =>
+// 	{
+// 		console.log( hash );
+// 	});
+// });
+
+var hashedPassword = '$2a$10$BBtuRRFaUAwGplvSbiJ9EeO0ys9IHZLs589PorvYn45ponnqe8dgK';
+bcrypt.compare( 'password', hashedPassword, ( err, res ) =>
+{
+	console.log( res );
+});
+
+/**
 var data =
 {
 	id: 10
@@ -11,7 +29,7 @@ console.log( token );
 
 var decoded = jwt.verify( token, '123abc' );
 console.log( 'decoded', decoded );
-
+*/
 /**
 var message = 'I am user number 2';
 var hash = SHA256( message ).toString();
